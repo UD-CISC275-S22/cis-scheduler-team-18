493fc1a6729bb984eb3d81a83bdfc57bc28abdb1
mycahdt <mycahdt@udel.edu>
2022-04-05 15:53:24 -0400

    added Mycah

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App(): JSX.Element {
             <header className="App-header">Team 18 Page</header>
             <div>Abbey Walters</div>
             <div>Brielle Hina</div>
+            <div>Mycah Detorres</div>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
