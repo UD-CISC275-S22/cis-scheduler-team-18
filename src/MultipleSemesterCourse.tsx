282c205fb3adc1cb6218e9c51901d7069405d734
awrose <79182069+awrose@users.noreply.github.com>
2022-04-07 16:19:33 -0400

    Table Element

--- src/MultipleSemesterCourse.tsx
+++ src/MultipleSemesterCourse.tsx
@@ -4,7 +4,20 @@ import { Plan } from "./interfaces/plan";
 import { Semester } from "./interfaces/semester";
 import { Course } from "./interfaces/course";
 
-export function MultipleSemesterCourses({}:{}): JSX.Element{
-    
+export function MultipleSemesterCourses({ plan }: { plan: Plan }): JSX.Element {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <td>This is the title</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>This is the Body</td>
+                </tr>
+            </tbody>
+        </table>
 
+    );
 }
