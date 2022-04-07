843db513d1d117bd02034ac2c3114ad7ab79fd0a
awrose <79182069+awrose@users.noreply.github.com>
2022-04-07 15:49:01 -0400

    created a file for this... about to work on another part

---
+++ src/MultipleSemesterCourse.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Stack } from "react-bootstrap";
+import { Plan } from "./interfaces/plan";
+import { Semester } from "./interfaces/semester";
+import { Course } from "./interfaces/course";
+
+export function MultipleSemesterCourses({}:{}): JSX.Element{
+    
+
+}
