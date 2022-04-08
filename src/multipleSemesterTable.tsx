014b88c58d6b5d4b8aafc87105dcf0ea16666e95
awrose <79182069+awrose@users.noreply.github.com>
2022-04-08 18:33:33 -0400

    this is a lot better; not exactly how I want it, there's still some weird styling things; but this is the general idea

--- src/multipleSemesterTable.tsx
+++ src/multipleSemesterTable.tsx
@@ -1,33 +1,33 @@
 import React from "react";
-import { Table } from "react-bootstrap";
 import { Plan } from "./interfaces/plan";
 import { Semester } from "./interfaces/semester";
 import { Course } from "./interfaces/course";
-import { CourseList } from "./CourseList";
-import { Z_ASCII } from "zlib";
-import { SingleSemester } from "./SingleSemester";
+import "./multipleSemesterTable.css";
 
 export function MultipleSemesterTable({ plan }: { plan: Plan }): JSX.Element {
     const SEMESTER = plan.semesters.map((sem: Semester) => ({ ...sem }));
     return (
-        <div>
+        <div className="container">
             {SEMESTER.map((sem: Semester) => (
-                <Table key={sem.id}>
-                    <thead>
-                        <th>Course name</th>
-                        <th>Course Title</th>
-                        <th>Credits</th>
-                    </thead>
-                    <tbody>
-                        {sem.courses.map((course: Course) => (
-                            <tr key={course.id}>
-                                <td>{course.courseName}</td>
-                                <td>{course.courseTitle}</td>
-                                <td>{course.credits}</td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </Table>
+                <div key={sem.id}>
+                    <h3 className="semester">{sem.id}</h3>
+                    <table className="table" key={sem.id}>
+                        <thead>
+                            <th>Course name</th>
+                            <th>Course Title</th>
+                            <th>Credits</th>
+                        </thead>
+                        <tbody className="body">
+                            {sem.courses.map((course: Course) => (
+                                <tr key={course.id}>
+                                    <td>{course.courseName}</td>
+                                    <td>{course.courseTitle}</td>
+                                    <td>{course.credits}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
             ))}
         </div>
     );
