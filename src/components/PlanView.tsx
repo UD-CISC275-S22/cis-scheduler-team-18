87893675ea5ca39814869aca4cfa0dcc52efb45c
mycahdt <mycahdt@udel.edu>
2022-04-11 08:46:04 -0400

    added planView calls Sems

---
+++ src/components/PlanView.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { Plan } from "../interfaces/plan";
+import { MultipleSemesterTable } from "./multipleSemesterTable";
+//import { PlanEditor } from "./PlanEditor";
+
+/*
+Add this later!
+export function PlanView({
+    plan,
+    deletePlan,
+    editPlan
+}: {
+    plan: Plan;
+    deletePlan: (id: string) => void;
+    editPlan: (id: string, newPlan: Plan) => void;
+}): JSX.Element {
+
+
+    ...
+    return editing ? (
+        <PlanEditor
+            changeEditing={changeEditing}
+            plan={plan}
+            editPlan={editPlan
+            deletePlan={deletePlan}
+        ></PlanEditor>
+    ) : (
+
+*/
+
+export function PlanView({ plan }: { plan: Plan }): JSX.Element {
+    const [editing, setEditing] = useState<boolean>(false);
+
+    function changeEditing() {
+        setEditing(!editing);
+    }
+
+    return editing ? (
+        <div>PlanEditor will go here!!!! {changeEditing}</div>
+    ) : (
+        <div>
+            <div>
+                {" "}
+                <h3>{plan.name}</h3>{" "}
+            </div>
+            <Container>
+                <Row>
+                    <Col>
+                        <MultipleSemesterTable
+                            plan={plan}
+                        ></MultipleSemesterTable>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
+}
