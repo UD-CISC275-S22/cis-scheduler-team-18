e3bff20de13e328e9bee4babddc2ac226e3978d4
awrose <79182069+awrose@users.noreply.github.com>
2022-04-12 16:12:27 -0400

    created component semesters that will take care of semester's functions

---
+++ src/components/addSemester.tsx
@@ -0,0 +1,6 @@
+export function addSemester() {
+    //this function will insert a semester into their plan
+    //fields in a semester
+    //insert a season, insert a year
+    //this is going to generate the id: Season + Year
+}
