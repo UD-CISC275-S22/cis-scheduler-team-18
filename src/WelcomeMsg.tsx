1abfabeabeb7bc66e081c4bd719fea14ca6fb9fe
Brielle Hina <bhina@udel.edu>
2022-04-12 14:03:14 -0400

    made a show/hide button

--- src/WelcomeMsg.tsx
+++ src/WelcomeMsg.tsx
@@ -7,18 +7,11 @@ export function Welcome(): JSX.Element {
     }
     function message(): JSX.Element {
         return (
-            <div style={border: "1px solid blue" ; text-align:"center"}>
-                <Form>
-                    <Col sm={2}>
-                        <Row sm={2}>
-                            Hello CS Student! Welcome to your scheduler! Here
-                            you can create multiple graduation plans to help you
-                            decide the best way for you to schedule your classes
-                            in future semesters. Click the Add Plan button below
-                            to begin.
-                        </Row>
-                    </Col>
-                </Form>
+            <div style={{ border: "1px solid blue" }}>
+                Hello CS Student! Welcome to your scheduler! Here you can create
+                multiple graduation plans to help you decide the best way for
+                you to schedule your classes in future semesters. Click the Add
+                Plan button below to begin.
             </div>
         );
     }
