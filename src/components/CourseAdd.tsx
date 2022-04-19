216c152f15168355ab88bd15328c588d7d7b0734
awrose <79182069+awrose@users.noreply.github.com>
2022-04-19 16:26:58 -0400

    changing fields in courseAdd

--- src/components/CourseAdd.tsx
+++ src/components/CourseAdd.tsx
@@ -11,9 +11,9 @@ export function CourseAdd({
     const [code, setCode] = useState("NEW101");
     const [title, setTitle] = useState("NEW COURSE");
     const [credits, setCredits] = useState("0");
-    const [required, setRequired] = useState(false);
+    //const [required, setRequired] = useState(false);
     const [isPreReq, setIsPreReq] = useState("");
-    const [hasPreReq, setHasPreReq] = useState(false);
+    //const [hasPreReq, setHasPreReq] = useState(false);
     const [show, setShow] = useState(false);
 
     //update functions
@@ -26,15 +26,15 @@ export function CourseAdd({
     function addCredits(event: React.ChangeEvent<HTMLInputElement>) {
         setCredits(event.target.value);
     }
-    function addRequired(event: React.ChangeEvent<HTMLInputElement>) {
-        setRequired(event.target.checked);
-    }
+    //function addRequired(event: React.ChangeEvent<HTMLInputElement>) {
+    //    setRequired(event.target.checked);
+    //}
     function addIsPreReq(event: React.ChangeEvent<HTMLInputElement>) {
         setIsPreReq(event.target.value);
     }
-    function addHasPreReq(event: React.ChangeEvent<HTMLInputElement>) {
-        setHasPreReq(event.target.checked);
-    }
+    //function addHasPreReq(event: React.ChangeEvent<HTMLInputElement>) {
+    //    setHasPreReq(event.target.checked);
+    //}
     //this function creates a new Course with the current given information and puts it in the course list
     function makeCourse() {
         const newCourse: Course = {
@@ -86,19 +86,25 @@ export function CourseAdd({
                     <Form.Group controlId="formCredits">
                         <Form.Label>Credits:</Form.Label>
                         <Form.Control
-                            type="number"
                             value={credits}
                             onChange={addCredits}
                         ></Form.Control>
                     </Form.Group>
-                    <Form.Check
+                    <Form.Group controlId="formPreReq">
+                        <Form.Label>PreReq To: </Form.Label>
+                        <Form.Control
+                            value={isPreReq}
+                            onChange={addIsPreReq}
+                        ></Form.Control>
+                    </Form.Group>
+                    {/*<Form.Check
                         type="checkbox"
                         id="is-required-check"
                         label="Required Course?"
                         checked={required}
                         onChange={addRequired}
-                    />
-                    <Form.Check
+    />*/}
+                    {/*<Form.Check
                         type="checkbox"
                         id="is-preReq"
                         label="Is this course a pre req?"
@@ -111,7 +117,7 @@ export function CourseAdd({
                         label="Does this course have a pre req?"
                         checked={hasPreReq}
                         onChange={addHasPreReq}
-                    />
+/>*/}
                     <Modal.Footer>
                         <Button variant="warning" onClick={close}>
                             Cancel
