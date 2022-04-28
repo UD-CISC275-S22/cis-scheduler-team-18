01dd03cfa745796af437a5cf9362a7577e00b422
Brielle Hina <bhina@udel.edu>
2022-04-28 17:32:17 -0400

    created arrays of course requirements

--- src/components/CourseInfo.tsx
+++ src/components/CourseInfo.tsx
@@ -1,6 +1,18 @@
 import React, { useState } from "react";
 import { Button, Modal } from "react-bootstrap";
 import { Course } from "../interfaces/course";
+import coreReqs from "../data/coreMajorRequirements.json";
+import techReqs from "../data/techElect.json";
+import scienceReq from "../data/scienceRequirement.json";
+import multiCultReq from "../data/multiCulturalReq.json";
+import DLEReq from "../data/DLEReq.json";
+import ai from "../data/ai.json";
+import bioInformatics from "../data/bioinformatics.json";
+import cybersecurity from "../data/cybersecurity.json";
+import dataScience from "../data/dataScience.json";
+import highPerformance from "../data/highPerf.json";
+import systemsNetwork from "../data/systemsNetworks.json";
+import theoryandComp from "../data/TheoryandComputation.json";
 /**
  * Displays a popup which lists all course information not seen on main page
  */
@@ -10,6 +22,57 @@ export function CourseInfo({ course }: { course: Course }): JSX.Element {
     //for opening/closing popup
     const close = () => setShow(false);
     const open = () => setShow(true);
+
+    //prints degree requirements this coure fufills
+    function degreeReq(): string {
+        //core requirements: an array of Course Objects that are the coreReqs -- THESE NEED TO BE TAKEN
+        const CORES = coreReqs.map(
+            (course: Course): Course => ({
+                ...course
+            })
+        );
+        //DLEReqs: an array of Course Objects that are DLE options -- ANY OF THESE CAN BE TAKEN (3 credits)
+        const DLE = DLEReq.map((course: Course): Course => ({ ...course }));
+        //multiculturalREQ: ONE of these classes need to be taken (3 credits)
+        const MULTICULTURAL = multiCultReq.map(
+            (course: Course): Course => ({ ...course })
+        );
+        //science requirement: a sequence is needed + additional (12 credits in total)
+        const SCIENCE = scienceReq.map(
+            (course: Course): Course => ({ ...course })
+        );
+        //techElect: 6 credits any 2 of these
+        const TECHELECT = techReqs.map(
+            (course: Course): Course => ({ ...course })
+        );
+        //ai concentration
+        const AI = ai.map((course: Course): Course => ({ ...course }));
+        //bioInform concentrat
+        const BIOINFORMATICS = bioInformatics.map(
+            (course: Course): Course => ({ ...course })
+        );
+        //cyberSecurity concentrat
+        const CYBERSECURITY = cybersecurity.map(
+            (course: Course): Course => ({ ...course })
+        );
+        //dataScience
+        const DATASICENCE = dataScience.map(
+            (course: Course): Course => ({ ...course })
+        );
+        //highPerformance
+        const HIGHPERFORMANCE = highPerformance.map(
+            (course: Course): Course => ({ ...course })
+        );
+        //systemsNetwork
+        const SYSTEMSNETWORK = systemsNetwork.map(
+            (course: Course): Course => ({ ...course })
+        );
+        //theoryandComp
+        const THEORYANDCOMP = theoryandComp.map(
+            (course: Course): Course => ({ ...course })
+        );
+        return "hello";
+    }
     return (
         <>
             <Button variant="secondary" onClick={open}>
