3c9a2aa93e2ac27b9b40ebe59b2b3531b33e594a
awrose <awrose@udel.edu>
2022-05-07 00:53:05 -0400

    figured out which tests are needed for CheckDegreeRequirements

--- src/components/CheckDegreeRequirements.test.tsx
+++ src/components/CheckDegreeRequirements.test.tsx
@@ -4,12 +4,47 @@
 //does it update when you delete a course?
 //does it update when you edit a course?
 import React from "react";
-import { fireEvent, render, screen } from "@testing-library/react";
-import { CheckDegreeReqs } from "./checkDegreeReqs";
-import userEvent from "@testing-library/user-event";
+import { render, screen } from "@testing-library/react";
+import { CheckDegreeReq } from "./CheckDegreeReqs";
 
 describe("MultipleSemester Component tests", () => {
     beforeEach(() => {
-        render(<CheckDegreeReqs />);
+        render(<CheckDegreeReq plan={} />);
+    });
+    test("All Concentrations are available", () => {
+
+    });
+    test("You can Choose your Concentration", () => {
+
+    });
+    test("Missing Core Reqs", () => {
+
+    });
+    test("Missing Tech Elects", () => {
+
+    });
+    test("Missing DLE", () => {
+
+    });
+    test("Missing Capstone", () => {
+
+    });
+    test("Missing Multicultural", () => {
+
+    });
+    test("Missing University Breadths", () => {
+
+    });
+    test("Missing COE Breadths", () => {
+
+    });
+    test("Missing Science Sequence", () => {
+
+    });
+    test("Missing Additional Science", () => {
+
+    });
+    test("Not enough Credits", () => {
+
     });
 });
