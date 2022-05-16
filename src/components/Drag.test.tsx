547ea98ec219c00a003b3dec6aae61189c5a3fce
mycahdt <mycahdt@udel.edu>
2022-05-15 20:17:58 -0400

    more drag tests

--- src/components/Drag.test.tsx
+++ src/components/Drag.test.tsx
@@ -14,11 +14,34 @@ describe("PlanEditor Component tests", () => {
     test("There is a select box", () => {
         expect(screen.getByRole("combobox")).toBeInTheDocument();
     });
-    test("Can choose a course and add it to the pool", () => {
+    test("a) Can choose a course and add it to the pool", () => {
         const select = screen.getByRole("combobox");
         userEvent.selectOptions(select, "CISC220");
         expect(
             screen.getByText(/CISC220: Data Structures/i)
+        ).toBeInTheDocument();
+    });
+    test("b) Can choose a course and add it to the pool", () => {
+        const select = screen.getByRole("combobox");
+        userEvent.selectOptions(select, "CISC260");
+        expect(
+            screen.getByText(
+                /CISC260: Machine Organization and Assembly Language/i
+            )
+        ).toBeInTheDocument();
+    });
+    test("c) Can choose a course and add it to the pool", () => {
+        const select = screen.getByRole("combobox");
+        userEvent.selectOptions(select, "CISC210");
+        expect(
+            screen.getByText(/CISC210: Introduction to Systems Programming/i)
+        ).toBeInTheDocument();
+    });
+    test("d) Can choose a course and add it to the pool", () => {
+        const select = screen.getByRole("combobox");
+        userEvent.selectOptions(select, "CISC275");
+        expect(
+            screen.getByText(/CISC275: Introduction to Software Engineering/i)
         ).toBeInTheDocument();
     });
 });
