900cc20fdb092d8ee39cc6a81e257b36f105d91e
awrose <awrose@udel.edu>
2022-05-16 00:17:31 -0400

    multipleSemesterTable test file started

--- src/components/MultipleSemesterTable.test.tsx
+++ src/components/MultipleSemesterTable.test.tsx
@@ -1,3 +1,37 @@
 //semester season and year
 //calls courselist
 //....figure this out
+
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { MultipleSemesterTable } from "./multipleSemesterTable";
+import semesterPlan from "../data/semesterPlan.json";
+import { Plan } from "../interfaces/plan";
+
+const plan = semesterPlan[0];
+const sem = plan.semesters[0];
+const plans = semesterPlan.map((plan: Plan): Plan => plan);
+
+describe("MultipleSemester Component Tests", () => {
+    beforeEach(() => {
+        render(
+            <MultipleSemesterTable
+                plan={plan}
+                semester={sem}
+                plans={plans}
+                setPlans={function (): void {
+                    throw new Error("Function not implemented.");
+                }}
+                setSemesters={function (): void {
+                    throw new Error("Function not implemented.");
+                }}
+                setData={function (): void {
+                    throw new Error("Function not implemented.");
+                }}
+            />
+        );
+    });
+    test("Work on this", () => {
+        //to do
+    });
+});
