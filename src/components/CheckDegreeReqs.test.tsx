be28a28f9b5fafd809969a63d8e41b1d336721ed
awrose <awrose@udel.edu>
2022-05-16 00:19:45 -0400

    CheckDegreeReqs tests

--- src/components/CheckDegreeReqs.test.tsx
+++ src/components/CheckDegreeReqs.test.tsx
@@ -1,2 +1,18 @@
 //this is going to be extremely long
 //checks to see all missing requirements in the document
+
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { CheckDegreeReq } from "./CheckDegreeReqs";
+import semesterPlan from "../data/semesterPlan.json";
+
+const plan = semesterPlan[0];
+
+describe("CheckDegreeReqs Component Tests", () => {
+    beforeEach(() => {
+        render(<CheckDegreeReq plan={plan} />);
+    });
+    test("Figure out what to test", () => {
+        //to do
+    })
+});
